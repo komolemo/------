c3ce04d0dfee725deb508fbf04420528bb4f7fc7
Nao <131878827+komolemo@users.noreply.github.com>
2023-08-16 17:39:01 +0900

    Update summarize.py

--- script/summarize.py
+++ script/summarize.py
@@ -4,14 +4,6 @@
 # # APIキーの設定
 # openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# response = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "user", "content": "大谷翔平について教えて"},
-#     ],
-# )
-# print(response.choices[0]["message"]["content"].strip())
-
 import os
 os.environ["OPENAI_API_KEY"] = "OpenAIのAPIコード"
 
